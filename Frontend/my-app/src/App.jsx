c9be813e521fbb6743c10ce21a0c8261c64908dd
bhawani mokawat <bhawanimokawat599@gmail.com>
2026-06-21 19:41:03 +0530

    Fix Dashboard import path

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -19,7 +19,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
- import Dashboard from "./pages/Dasboard";
+ import Dashboard from "./pages/Dashboard";
  import ProtectedRoute from "./components/routes/ProtectRoutes";
 
 function App() {
